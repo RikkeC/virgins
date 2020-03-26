032af2dd9cdcc6d8676475f698a1e498da9422a6
RikkeChristiansen <rikk702t@stud.kea.dk>
2020-03-26 20:22:13 +0100

    om section done

--- virgins/js/scripts.js
+++ virgins/js/scripts.js
@@ -1,18 +1,18 @@
-$(function () {
-    $(window).on('load', function () {
-        $('.parallax-window').parallax({});
+ $(window).on('load', function () {
+     $('.parallax-window').parallax({});
+ });
+ $(function () {
+     $.instagramFeed({
+         'username': 'instagram',
+         'container': "#my-instagram-feed",
+         'display_profile': true,
+         'display_biography': false,
+         'display_gallery': true,
+         'callback': null,
+         'styling': true,
+         'items': 6,
+         'items_per_row': 3,
+         'margin': 2
 
-        $.instagramFeed({
-            'username': 'instagram',
-            'container': "#my-instagram-feed",
-            'display_profile': true,
-            'display_biography': false,
-            'display_gallery': true,
-            'callback': null,
-            'styling': true,
-            'items': 6,
-            'items_per_row': 3,
-            'margin': 2
-        });
-    });
-})(jQuery);
+     });
+ }(jQuery));
